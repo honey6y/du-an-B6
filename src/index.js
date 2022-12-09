0fd73b01555fca4249be5d6fdd1a89e31e68de41
honey6y~ <thaivtvp@gmail.com>
2022-12-09 21:36:13 +0700

    xoa file du thua, them file GlobalStyle.scss

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import RouterElement from './RouterElement';
-
+import GlobalStyle from './component/GlobalStyles';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterElement>
-    <App />
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
     </RouterElement>
   </React.StrictMode>
 );
