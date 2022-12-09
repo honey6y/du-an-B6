68fd5238d26e8abac1a458d48b60c33679751b1c
chattq <baotuyet927@gmail.com>
2022-12-09 21:54:34 +0700

    chat them moi

--- src/component/accout/login/Login.jsx
+++ src/component/accout/login/Login.jsx
@@ -10,31 +10,33 @@ function Login () {
                     <span className={style.seperate}>/</span>
                     <span className={style.nameLogin}>Tài khoản</span>
                 </div>
-                <div className={style.login_body}>
-                    <form className={style.login_form}>
-                        <div className={style.login_title}>Đăng Nhập</div>
-                        <div className={style.seperate_line}/>
-                        <div>
-                            <input type="text"  placeholder="Email"/>
-                            <div className={style.email_error}></div>
-                        </div>
-                        <div>
-                            <input type="password" />
-                            <div className={style.passwordl_error}></div>
-                        </div>
-                        <div>
-                        <button className={style.button_login}>Đăng nhập</button>
-                        </div>
-                        <p>
-                            <Link to='/'>Trở về</Link>
-                        </p>
-                        <p>
-                            <Link to='/'>Đăng kí</Link>
-                        </p>
-                        <p>
-                            <Link to='/'>Quên Mật Khẩu</Link>
-                        </p>
-                    </form>
+                <div className={style.login_body_container}>
+                    <div className={style.login_body}>
+                        <form className={style.login_form}>
+                            <div className={style.login_title}>Đăng Nhập</div>
+                            <div className={style.seperate_line}/>
+                            <div>
+                                <input type="text"  placeholder="Email"/>
+                                <div className={style.email_error}></div>
+                            </div>
+                            <div>
+                                <input type="password" />
+                                <div className={style.passwordl_error}></div>
+                            </div>
+                            <div>
+                            <button className={style.button_login}>Đăng nhập</button>
+                            </div>
+                            <p>
+                                <Link to='/'>Trở về</Link>
+                            </p>
+                            <p>
+                                <Link to='/'>Đăng kí</Link>
+                            </p>
+                            <p>
+                                <Link to='/'>Quên Mật Khẩu</Link>
+                            </p>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
