5e6404f7092258c0d024be7d413c949ef8376d61
An <An@example.com>
2022-12-12 17:35:51 +0700

    cart page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import RouterElement from './RouterElement';
-import GlobalStyle from './component/GlobalStyles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import RouterElement from "./RouterElement";
+import GlobalStyle from "./component/GlobalStyles";
+import store from "./store";
+import { Provider } from "react-redux";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterElement>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
-    </RouterElement>
+    <Provider store={store}>
+      <RouterElement>
+        <GlobalStyle>
+          <App />
+        </GlobalStyle>
+      </RouterElement>
+    </Provider>
   </React.StrictMode>
 );
 
