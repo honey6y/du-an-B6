5df5bb1ed5fce40df5f4688c6dafc784427c25d0
chattq <baotuyet927@gmail.com>
2022-12-28 21:27:00 +0700

    quang update category

--- src/component/product-Category/containerProductCt/ContainerPdCategory.jsx
+++ src/component/product-Category/containerProductCt/ContainerPdCategory.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import { Row, Col } from "antd";
 import styles from "./ContainerPdCategory.module.scss";
@@ -19,36 +19,33 @@ export default function ContainerPdCategory({ data }) {
           <Row gutter={[10, 10]}>
             {data === null ? <p>Đang tải dữ liệu</p> : data.length !== 0 ? data.map((item, index) => {
               return (
-                <Col span={4} key={item._id}>
-                  <div className={cx("product")}>
-                    <LazyLoadImage
+                <Col xs={24} sm={12} md={8} lg={6} xl={4} key={item._id}>
+                    <div className={cx("product")} >
+                      <LazyLoadImage 
                       className={cx("product-img")}
-                      src={item.thump}
+                      src={item.thump[0]}
                       alt=""
                       effect="blur"
                       placeholderSrc="https://dummyimage.com/640x360/fff/aaa
                       "
-                      onClick={() => nav(`/detail/${item._id}`)}
-                    />
-                    <div>
-                      <p
-                        className={cx("product-name")}
-                        onClick={() => nav(`/detail/${item._id}`)}>
-                        {item.productName}
-                      </p>
-                      <h4 className={cx("product-price")}>
-                        {item.price && item.price.toLocaleString()}&nbsp;đ
-                      </h4>
-                      <button
-                        className={cx("btn-buy")}
-                        onClick={() => {
-                          dispatch(openModal(item));
-                        }}>
-                        <span>Mua</span>
-                      </button>
+                      onClick={()=>nav(`/detail/${item._id}`)}
+                      />
+                      <div>
+                        <p className={cx("product-name")} onClick={()=>nav(`/detail/${item._id}`)}>{item.productName}</p>
+                        <h4 className={cx("product-price")}>
+                          {item.price && item.price.toLocaleString()}&nbsp;đ
+                        </h4>
+                        <button
+                          className={cx("btn-buy")}
+                          onClick={() => {
+                            dispatch(openModal(item))
+                          }}
+                        >
+                          <span>Mua</span>
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                </Col>
+                  </Col>
               );
               }) : <ProductError />
             } 
